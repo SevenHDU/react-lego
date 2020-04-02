3219667b07cdeeeab4940c5fec5837933227bbe4
Seven <sjj233788@alibaba-inc.com>
2020-04-03 02:23:44 +0800

    feat: 增加代码规范

--- client/src/app.js
+++ client/src/app.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 function app() {
+    const a = 'dsada';
+    console.log(a);
+
     return (
         <div>
             <p>111111111</p>
         </div>
-    )
-};
+    );
+}
 
 export default hot(app);
